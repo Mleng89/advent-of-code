8ecdb5de257ce69bfb39d2a3bfdede28f6c6c17e
Matthew Leng <mleng89@gmail.com>
2021-12-09 15:01:19 -0500

    Updated: Day 2

--- 2021 advent/Day 2/day2.js
+++ 2021 advent/Day 2/day2.js
@@ -15,27 +15,60 @@ const submarine = fs
 let horizontal = 0;
 let depth = 0;
 
+// for (let i = 0; i < submarine.length; i++) {
+// 	if (submarine[i].includes('forward')) {
+// 		let newSub = submarine[i].slice(8);
+// 		let numSub = parseInt(newSub);
+// 		horizontal += numSub;
+// 	}
+// 	if (submarine[i].includes('down')) {
+// 		let newDown = submarine[i].slice(5);
+// 		let numDown = parseInt(newDown);
+// 		depth += numDown;
+// 	} else if (submarine[i].includes('up')) {
+// 		let newUp = submarine[i].slice(3);
+// 		let numUp = parseInt(newUp);
+// 		depth -= numUp;
+// 	}
+// 	console.log('my horizontal is:', horizontal);
+// 	console.log('my depth is:', depth);
+
+// 	let result = depth * horizontal;
+// 	// console.log(result);
+// }
+
+// ******PART 2***********
+//Down x increases aim
+//Up x decreases aim
+//Forward x (does 2 things)
+// a. increases horizontal position by x units
+// b. increases depth by aim multiplied by x
+
+let aim = 0;
+
 for (let i = 0; i < submarine.length; i++) {
 	if (submarine[i].includes('forward')) {
 		let newSub = submarine[i].slice(8);
 		let numSub = parseInt(newSub);
 		horizontal += numSub;
+		depth += numSub * aim;
 	}
 	if (submarine[i].includes('down')) {
 		let newDown = submarine[i].slice(5);
 		let numDown = parseInt(newDown);
-		depth += numDown;
+		aim += numDown;
 	} else if (submarine[i].includes('up')) {
 		let newUp = submarine[i].slice(3);
 		let numUp = parseInt(newUp);
-		depth -= numUp;
+		aim -= numUp;
 	}
 	console.log('my horizontal is:', horizontal);
 	console.log('my depth is:', depth);
-
+	console.log('my aim is:', aim);
 	let result = depth * horizontal;
-	// console.log(result);
-}
 
-// ******PART 2***********
-// Down x increases
+	console.log('part 2 result:', result);
+}
+// let result = depth * horizontal;
+
+// 	console.log('part 2 result:', result);
